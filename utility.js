860e7040a38b9d6de04ca656ea4d1bcf26a51d22
NotHydra <rizky.irswanda115@gmail.com>
2022-09-11 22:13:21 +0800

    feat: random unique integer

--- utility.js
+++ utility.js
@@ -1,3 +1,19 @@
 export function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function randomUniqueInteger(min, max, amount) {
+    const randomUniqueIntegerArray = [];
+
+    for (let i = 0; i < amount; i++) {
+        let randomIntergerValue = randomInteger(min, max);
+
+        if (!randomUniqueIntegerArray.includes(randomIntergerValue)) {
+            randomUniqueIntegerArray.push(randomIntergerValue);
+        } else if (randomUniqueIntegerArray.includes(randomIntergerValue)) {
+            i--;
+        }
+    }
+
+    return randomUniqueIntegerArray;
+}
