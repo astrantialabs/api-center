1a9647c5508776c6de49be8a4b0cd41f4b4dc268
NotHydra <rizky.irswanda115@gmail.com>
2022-09-13 18:10:19 +0800

    feat: api route

--- app.js
+++ app.js
@@ -163,3 +163,83 @@ app.get("/random/student/sma/amount/:amount", (req, res) => {
 });
 
 //#endregion Random
+
+//#region API Static
+
+app.get("/api/static/name", (req, res) => {
+    const responseObject = nameArray.map((name, nameIndex) => {
+        return { id: nameIndex + 1, name: name };
+    });
+
+    res.json(responseObject);
+});
+
+app.get("/api/static/birth-place", (req, res) => {
+    const responseObject = birthPlaceArray.map((birthPlace, birthPlaceIndex) => {
+        return { id: birthPlaceIndex + 1, birthPlace: birthPlace };
+    });
+
+    res.json(responseObject);
+});
+
+app.get("/api/static/religion", (req, res) => {
+    const responseObject = religionArray.map((religion, religionIndex) => {
+        return { id: religionIndex + 1, religion: religion };
+    });
+
+    res.json(responseObject);
+});
+
+app.get("/api/static/smk-major", (req, res) => {
+    const responseObject = smkMajorArray.map((smkMajor, smkMajorIndex) => {
+        return { id: smkMajorIndex + 1, smkMajor: smkMajor };
+    });
+
+    res.json(responseObject);
+});
+
+app.get("/api/static/sma-major", (req, res) => {
+    const responseObject = smaMajorArray.map((smaMajor, smaMajorIndex) => {
+        return { id: smaMajorIndex + 1, smaMajor: smaMajor };
+    });
+
+    res.json(responseObject);
+});
+
+//#endregion Static
+
+//#region API Random
+
+app.get("/api/random/coinflip", (req, res) => {
+    res.json({ id: 1, type: "coinflip", value: randomInteger(0, 1) });
+});
+
+app.get("/api/random/dice", (req, res) => {
+    res.json({ id: 1, type: "dice", value: randomInteger(1, 6) });
+});
+
+app.get("/api/random/student/smk", (req, res) => {
+    const responseObject = randomStudentSMK(100);
+
+    res.json(responseObject);
+});
+
+app.get("/api/random/student/smk/amount/:amount", (req, res) => {
+    const responseObject = randomStudentSMK(parseInt(req.params.amount));
+
+    res.json(responseObject);
+});
+
+app.get("/api/random/student/sma", (req, res) => {
+    const responseObject = randomStudentSMA(100);
+
+    res.json(responseObject);
+});
+
+app.get("/api/random/student/sma/amount/:amount", (req, res) => {
+    const responseObject = randomStudentSMA(parseInt(req.params.amount));
+
+    res.json(responseObject);
+});
+
+//#endregion Random
